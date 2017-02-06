9d5a266f87471abf015ac25ed07a13a5bebcd930
Dylansweaver <dylanweaver@Dylans-Mac.local>
2017-02-05 23:34:50 -0600

    TicTacToe290

--- TicTacToe290/ViewController.swift
+++ TicTacToe290/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TextChange: UILabel!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let title = sender.title(for: .selected)!
+        let text = "\(title)"
+        TextChange.title = text
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
